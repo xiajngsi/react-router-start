f877102432bbccea5662d87701e69ab9c2b2e9c9
candice <xiajingsi00@163.com>
2017-08-10 19:42:54 +0800

    canvas draw smill face

--- app/canvas.jsx
+++ app/canvas.jsx
@@ -6,10 +6,41 @@ export default class CanvasDemo extends React.Component {
     const canvas = document.querySelector('#stockGraph')
     if (canvas.getContext) {
       const ctx = canvas.getContext('2d')
-      ctx.fillStyle = 'rgb(200,0,0)'
-      ctx.fillRect(10, 10, 55, 50)
-      ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
-      ctx.fillRect(30, 30, 55, 50)
+      ctx.beginPath()
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true)
+      ctx.moveTo(110, 75)
+      ctx.arc(75, 75, 35, 0, Math.PI, false)
+      ctx.moveTo(65, 65)
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true)
+      ctx.moveTo(95, 65)
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true)
+      ctx.stroke()
+
+
+      // ctx.beginPath()
+      // ctx.arc(75, 75, 50, 0, Math.PI * 2, true) // 绘制
+      // ctx.moveTo(110, 75)
+      // ctx.arc(75, 75, 35, 0, Math.PI, false) // 口(顺时针)
+      // ctx.moveTo(65, 65)
+      // ctx.arc(60, 65, 5, 0, Math.PI * 2, true) // 左眼
+      // ctx.moveTo(95, 65)
+      // ctx.arc(90, 65, 5, 0, Math.PI * 2, true) // 右眼
+      // ctx.stroke()
+
+      // ctx.beginPath()
+      // ctx.moveTo(75, 50)
+      // ctx.lineTo(100, 75)
+      // ctx.stroke(100, 25)
+      // ctx.fill()
+
+      // ctx.fillRect(25, 25, 100, 100)
+      // ctx.clearRect(45, 45, 60, 60)
+      // ctx.strokeRect(50, 50, 50, 50)
+
+      // ctx.fillStyle = 'rgb(200,0,0)'
+      // ctx.fillRect(10, 10, 55, 50)
+      // ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
+      // ctx.fillRect(30, 30, 55, 50)
       // drawing code here
     } else {
       message('要不换个浏览器？')
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-const path  = require('path')
+const path = require('path')
+
 module.exports = {
   devtool: 'inline-source-map',
   entry: './app/app.jsx',
   output: {
-    filename: 'bundle.js'
+
+    filename: 'bundle.js',
   },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js', '.jsx'] // note if using webpack 1 you'd also need a '' in the array as well
+    extensions: ['.ts', '.tsx', '.js', '.jsx'], // note if using webpack 1 you'd also need a '' in the array as well
   },
   module: {
     rules: [
@@ -17,23 +19,23 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
-          }
-        }
-      }
+            presets: ['env'],
+          },
+        },
+      },
     ],
     loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: 'ts-loader' },
       {
-          test: /\.jsx?$/,
-          include: [
-            path.resolve(__dirname, 'app'),
-          ],
-          loader: [
-            'babel-loader',
-          ],
-        },
-    ]
-  }
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'app'),
+        ],
+        loader: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
 }
